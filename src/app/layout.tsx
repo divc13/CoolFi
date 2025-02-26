59897b13c01d11e18fb3fe0df4b0cad1b4d1d2e7
Divyansh Chhabria <chhabriadivyansh@gmail.com>
2025-02-26 22:26:06 +0530

    Hello

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import "./global.css"; // Ensure this is correctly imported
 import { BitteWalletContextProvider } from "@bitte-ai/react";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const BitteWalletSetup = {
   network: "mainnet",
